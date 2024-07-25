c96eaa684374df385796cb9683cec0499e621ecd
erenyavuz02 <eyavuz21@ku.edu.tr>
2024-07-25 14:28:52 +0300

    notification app constructed, images for github added

---
+++ notification_app/src/test/java/com/erenyavuz/microservices/notification_app/NotificationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erenyavuz.microservices.notification_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
